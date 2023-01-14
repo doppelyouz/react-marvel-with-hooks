bd17d5df4a2e45cd2647eb0c816112a854386143
anikisana <younker12345@gmail.com>
2023-01-14 18:45:30 +0600

    helmet, seo

--- marvel-react/src/components/pages/ComicsPage.js
+++ marvel-react/src/components/pages/ComicsPage.js
@@ -2,9 +2,18 @@
 import AppBanner from '../appBanner/AppBanner';
 import ComicsList from '../comicsList/ComicsList';
 
+import { Helmet } from 'react-helmet';
+
 const ComicsPage = () => {
     return (
         <>
+            <Helmet>
+                <meta
+                    name="description"
+                    content="Page with list of our comics"
+                    />
+                <title>Comics Page</title>
+            </Helmet>
             <AppBanner />
             <ComicsList />
         </>
